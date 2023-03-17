26a294083889f827597e006d3b67f5801bb50aa6
bemnet16 <119627769+bemnet16@users.noreply.github.com>
2023-03-18 02:33:35 +0300

    Create LinkedListCycle_II.py
    leet code 142

---
+++ LinkedListCycle_II.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        fast = head
+        slow = head
+        while fast != None and fast.next != None:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
