e615b659fb75e1e93dac01b56c1dd76a08f64c50
bemnet16 <119627769+bemnet16@users.noreply.github.com>
2023-03-21 13:00:15 +0300

    leet code 103

---
+++ BinaryTreeLevelOrderTraverse.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def levelOrder(self, root):
+        if not root:
+            return []
+
+        stc = [root]
+        result = []
+        while stc:
+            temp = []
+            for i in range(len(stc)):
+                cur = stc.pop()
+                if cur:
+                    temp.append(cur.val)
+                    stc.insert(0,cur.left)
+                    stc.insert(0,cur.right)
+            if temp:
+                result.append(temp)
+        return result
