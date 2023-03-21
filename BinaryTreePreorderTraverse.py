f8dbcc504b0170919e04cf32a2c03c6659d039a7
bemnet16 <119627769+bemnet16@users.noreply.github.com>
2023-03-21 14:27:37 +0300

    leet code 144

--- BinaryTreePreorderTraverse.py
+++ BinaryTreePreorderTraverse.py
@@ -1,3 +1,5 @@
+### Recursiion
+
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
@@ -13,3 +15,28 @@ class Solution(object):
             self.preorderTraversal(root.left)
             self.preorderTraversal(root.right)
         return self.res
+
+ #### Iteration
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def preorderTraversal(self, root):
+        stc = [root]
+        res = []
+        s = root
+        while stc:
+            if s:
+                stc.append(s)
+                res.append(s.val)
+                s = s.left
+            else:
+                if len(stc) > 1:
+                    s = stc.pop().right
+                else:
+                    stc = []
+        return res
