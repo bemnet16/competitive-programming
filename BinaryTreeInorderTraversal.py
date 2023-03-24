1239a54f55aebb022bb9ffebfe3da6e6515ed4a9
bemnet16 <119627769+bemnet16@users.noreply.github.com>
2023-03-24 03:12:38 +0300

    leet code 94

--- BinaryTreeInorderTraversal.py
+++ BinaryTreeInorderTraversal.py
@@ -1,3 +1,5 @@
+#### Recursion
+
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
@@ -13,3 +15,38 @@ class Solution(object):
             self.l.append(root.val)
             self.inorderTraversal(root.right)
         return self.l 
+
+    
+    
+    ### Iterative way
+    
+    
+    # Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def __init__(self):
+        self.l = []
+    def inorderTraversal(self, root):
+        if not root:
+            return []
+        stc = [root]
+        res = []
+        s = root
+        while stc:
+            if s:
+                stc.append(s)
+                s = s.left
+            else:
+                cur = stc.pop()
+                if cur:
+                    res.append(cur.val)
+                    s = cur.right
+                else:
+                    if len(stc) == 1:
+                        stc = []
+        res.pop()
+        return res
