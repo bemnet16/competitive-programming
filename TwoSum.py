07367a656d1c0024d8ba7f88001aebb6768867ee
bemnet16 <119627769+bemnet16@users.noreply.github.com>
2023-04-04 17:20:10 +0300

    leet code 1

---
+++ TwoSum.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        # for i in range(len(nums)):
+        #     for j in range(i,len(nums)):
+        #         if i==j:continue
+        #         if nums[i]+nums[j]==target:
+        #             return [i,j]
+        
+        for i,val in enumerate(nums):
+            if (target-val) in nums[i+1:]:
+                return [i,nums[i+1:].index(target-val)+len(nums[:i+1])]
+            
+            
+                    
+        
