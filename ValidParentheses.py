33fd5bfb8ddff2cc83126691888046535fa720a1
bemnet16 <119627769+bemnet16@users.noreply.github.com>
2023-04-10 09:59:20 +0300

    leet code 20

--- ValidParentheses.py
+++ ValidParentheses.py
@@ -1,15 +1,13 @@
-class Solution:
-  def isValid(self, s: str) -> bool:
-    tem_li = []
-
-    for char in s:
-      if char == '(':
-        tem_li.append(')')
-      elif char == '{':
-        tem_li.append('}')
-      elif char == '[':
-        tem_li.append(']')
-      elif not tem_li or tem_li.pop() != char:
-        return False
-
-    return not tem_li
+class Solution(object):
+    def isValid(self, s):
+        stc=[]
+        for c in s:
+            if c=='(' or c=='[' or c=='{':
+                stc.append(c)
+            elif c==')' or c==']' or c=='}':
+                if not stc: return False
+                t=stc.pop()
+                if c==')' and t!='(': return False
+                elif c==']' and t!='[': return False
+                elif c=='}' and t!='{': return False
+        return not stc
