42f4f60b682bdcdb34e1165bcbb80bea5a994bbd
bemnet16 <119627769+bemnet16@users.noreply.github.com>
2023-04-29 23:22:58 +0300

    LeetCode 876

--- MiddleOfLinkedList.py
+++ MiddleOfLinkedList.py
@@ -1,12 +1,17 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
-        h1 = h2 = head
-        while h2 and (h2:= h2.next):
-            if h2: h2 = h2.next;h1= h1.next
-        return h1
+class Solution(object):
+    def middleNode(self, head):
+        cur=head
+        lg=1
+        while cur.next:
+            lg+=1
+            cur=cur.next
+        lg//=2
+        for i in range(lg):
+            head=head.next
+        return head
+            
