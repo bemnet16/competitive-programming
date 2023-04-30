4b67bc4697eb3939058e841f633cde78a8cffa55
bemnet16 <119627769+bemnet16@users.noreply.github.com>
2023-04-30 10:39:38 +0300

    LeetCode 25

---
+++ ReverseNodesink-Group.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseKGroup(self, head, k):
+        if k==1: return head
+        cur=head
+        lg=1
+        d={1:head}
+        while cur.next:
+            cur=cur.next
+            lg+=1
+            d[lg]=cur
+            
+        lg//=k
+        temp=d[lg*k].next
+        for i in range(lg*k):
+            d[i+1].next=None
+            
+        nn=ListNode()
+        cur=nn
+        for i in range(lg):
+            for j in range(k,0,-1):
+                cur.next=d[j+(k*i)]
+                cur=cur.next
+        cur.next=temp
+        return nn.next
+                
+                
