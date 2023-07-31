384cb4c88bf2c34bbb884638b3756f386268fd9b
bemnet16 <119627769+bemnet16@users.noreply.github.com>
2023-07-31 16:19:38 +0300

    leetCode 189

--- RotateArray.py
+++ RotateArray.py
@@ -1,6 +1,10 @@
 class Solution(object):
     def rotate(self, nums, k):
+        # k=k%len(nums)
+        # for i,v in enumerate(nums[len(nums)-k:]+nums[:len(nums)-k]):
+        #     nums[i]=v
+        # return nums
+        
         k=k%len(nums)
-        for i,v in enumerate(nums[len(nums)-k:]+nums[:len(nums)-k]):
-            nums[i]=v
-        return nums
+        nums[:]=nums[len(nums)-k:]+nums[:len(nums)-k]
+        
