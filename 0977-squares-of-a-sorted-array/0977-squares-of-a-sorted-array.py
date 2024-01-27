2c68cde047293624b3d424b130e4c588ab7a5793
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-01-27 23:29:53 +0300

    Time: 157 ms (34.68%), Space: 13.7 MB (97.60%) - LeetHub

---
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def sortedSquares(self, nums):
+        res = []
+        l,r = 0,len(nums) - 1
+        for i in range(len(nums)):
+            if abs(nums[l]) > abs(nums[r]):
+                res.append(nums[l] ** 2)
+                l += 1
+            else:
+                res.append(nums[r] ** 2)
+                r -= 1
+        return res[::-1]
