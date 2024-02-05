82f1ce1c4f1c11743647355415a0664ed41fb982
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-02-05 14:32:48 +0300

    Time: 923 ms (5.44%), Space: 12.4 MB (91.05%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -0,0 +1,63 @@
+class Solution(object):
+    def minWindow(self, s, t):
+        if len(t) > len(s):
+            return ""
+        
+        ls,lt = len(s),len(t)
+        st = set(t)
+        dt = {}
+        for char in t:
+            dt[char] = dt.get(char,0) + 1
+        
+            
+        res = ""
+        sr = set(s[0:lt])
+        lr = float("inf")
+        
+        dr = {}
+        for char in s[0:lt]:
+            dr[char] = dr.get(char,0) + 1
+
+        i,j = lt - 1,0
+        while i < ls:
+            if st.issubset(sr):
+                isSub = all(dr.get(key,0) >= val for key,val in dt.items())
+                if isSub:
+                    if dr[s[j]] >= 2:
+                        dr[s[j]] -= 1
+                    elif dr[s[j]] == 1:
+                        del dr[s[j]]
+                        sr.discard(s[j])
+                    
+                    if (i - j) < lr:
+                        res = s[j:i + 1]
+                        lr = i - j
+                    j += 1
+                    
+                else:
+                    i += 1
+                    if i < ls:
+                        dr[s[i]] = dr.get(s[i],0) + 1
+                        sr.add(s[i])
+                
+            else:
+                i += 1
+                if i < ls:
+                    dr[s[i]] = dr.get(s[i],0) + 1
+                    sr.add(s[i])
+        
+        return res
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
