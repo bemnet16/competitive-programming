603c3512996976d135cd06bb716c3c9a08becb20
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-02-08 16:05:11 +0300

    Time: 1089 ms (87.35%), Space: 22.4 MB (96.73%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
@@ -1,17 +1,17 @@
 class NumMatrix(object):
 
     def __init__(self, matrix):
-        self.prefix = [[0] * (len(matrix[0]) + 1) for i in range(len(matrix) + 1)]
+        self.p_mat = [[0] * (len(matrix[0]) + 1) for i in range(len(matrix) + 1)]
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
-                self.prefix [i + 1][j + 1] = self.prefix[i][j + 1] + self.prefix[i + 1][j] - self.prefix[i][j] + matrix[i][j]
+                self.p_mat[i + 1][j + 1] = self.p_mat[i][j + 1] + self.p_mat[i + 1][j] + matrix[i][j] - self.p_mat[i][j]
 
     def sumRegion(self, row1, col1, row2, col2):
-        row1 += 1
-        row2 += 1
-        col1 += 1
-        col2 += 1
-        return self.prefix[row2][col2] - self.prefix[row2][col1 - 1] - self.prefix[row1 - 1][col2] + self.prefix[row1 - 1][col1 - 1]
+        ## p_mat add 1 col and 1 row on matrix
+        row1, col1 = row1 + 1, col1 + 1
+        row2, col2 = row2 + 1, col2 + 1
+        
+        return self.p_mat[row2][col2] - self.p_mat[row1 - 1][col2] - self.p_mat[row2][col1 - 1] + self.p_mat[row1 - 1][col1 - 1]
 
 
 # Your NumMatrix object will be instantiated and called as such:
