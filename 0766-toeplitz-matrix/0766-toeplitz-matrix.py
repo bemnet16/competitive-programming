4b151e9247ce117564d3c2abc806bf5b83c7e3d2
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-02-10 01:54:03 +0300

    Time: 52 ms (90.89%), Space: 11.5 MB (99.42%) - LeetHub

---
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isToeplitzMatrix(self, matrix):
+        
+        for i in range(len(matrix) - 1):
+            for j in range(len(matrix[0]) - 1):
+                if matrix[i][j] != matrix[i + 1][j + 1]:
+                    return False
+        
+        return True
