e7a1f1e2b37948b37327993886e3a27d91ec394e
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-02-12 16:00:50 +0300

    Add solution for Merge Two Sorted Lists

--- leetcode/merge-two-sorted-lists.py
+++ leetcode/merge-two-sorted-lists.py
@@ -1,27 +1,40 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def mergeTwoLists(self, list1, list2):
-        if not list1 and list2: return list2
-        l=ListNode()
-        l.next=list1
-        ucu=list1
-        utemp=l
-        lcu=list2
-        ltemp=list2
-        while lcu and ucu:
-            if ucu.val>lcu.val:
-                utemp.next=lcu
-                ltemp=ltemp.next
-                lcu.next=ucu
-                lcu=ltemp
-                utemp=utemp.next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        
+        # Start with a demmy node to append sorted list
+        demmy_node = ListNode()
+        
+        # Initialize the head of those three linked lists
+        demmy_head = demmy_node
+        head_1 = list1
+        head_2 = list2
+        
+        # Iterete until we get none for either of the linked list
+        while head_1 and head_2:
+            
+            # Find the minimum value and append the node to the demmy node
+            if head_1.val <= head_2.val:
+                demmy_head.next = head_1
+                head_1 = head_1.next
             else:
-                utemp=ucu
-                ucu=ucu.next
-        if lcu and utemp:
-            utemp.next=lcu
-        return l.next
+                demmy_head.next = head_2
+                head_2 = head_2.next
+            
+            # Update the demmy head to the appended node
+            demmy_head = demmy_head.next
+        
+        # Check if there is a remaining nodes from those two linked lists
+        if head_1:
+            demmy_head.next = head_1
+        else:
+            demmy_head.next = head_2
+        
+        
+        return demmy_node.next
+        
+        
