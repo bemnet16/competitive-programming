3539b4b13fd96ba885bd73d78fb4bd1037fffd25
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-02-14 00:18:41 +0300

    Time: 103 ms (52.63%), Space: 16.6 MB (36.84%) - LeetHub

---
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def minimumSteps(self, s):
+        
+        answer = 0
+        suffix_sum = 0
+        
+        for i in range((len(s) - 1), -1, -1):
+            
+            # count the number of '0's 
+            if s[i] == '0':
+                suffix_sum += 1
+            
+            # every '1' should be swapped with every '0' on its right side
+            else:
+                answer += suffix_sum
+            
+        return answer
