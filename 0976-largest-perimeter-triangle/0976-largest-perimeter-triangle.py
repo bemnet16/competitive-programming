26fea66db73fb7c356a9723676ba9d5d88873c9f
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-02-14 11:05:33 +0300

    Time: 169 ms (34.33%), Space: 17.8 MB (82.79%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.py
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.py
@@ -4,7 +4,7 @@ class Solution:
         
         def canFormTriangle(side1,side2,side3):
             
-            if (side1 + side2) > side3 and (side2 + side3) > side1 and (side1 + side3) > side2:
+            if (side2 + side3) > side1:
                 return True
             return False
         
