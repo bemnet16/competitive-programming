46a0124112e11a1de2e0d4919eb361673da5efb9
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-02-16 11:38:19 +0300

    Time: 365 ms (73.94%), Space: 43.1 MB (92.55%) - LeetHub

--- 2526-find-consecutive-integers-from-a-data-stream/2526-find-consecutive-integers-from-a-data-stream.py
+++ 2526-find-consecutive-integers-from-a-data-stream/2526-find-consecutive-integers-from-a-data-stream.py
@@ -3,37 +3,39 @@ from collections import deque
 class DataStream:
 
     def __init__(self, value: int, k: int):
+        
         self.value = value
         self.k = k
-        # self.left = 0
-        # self.right = 0
-        self.deque = deque()
-        self.count = 0
         
+        # self.deque = deque()
+        # self.count = 0
+        
+        self.left = 0
+        self.right = 0
 
     def consec(self, num: int) -> bool:
         
-        if len(self.deque) == self.k:
-            left_ele = self.deque.popleft()
-            if left_ele == self.value:
-                self.count -= 1
+#         if len(self.deque) == self.k:
+#             left_ele = self.deque.popleft()
+#             if left_ele == self.value:
+#                 self.count -= 1
         
-        self.deque.append(num)
-        if num == self.value:
-            self.count += 1
+#         self.deque.append(num)
+#         if num == self.value:
+#             self.count += 1
         
-        return self.count == self.k
+#         return self.count == self.k
         
-#         if self.value == num:
-#             self.right += 1
+        if self.value == num:
+            self.right += 1
         
-#         else:
-#             self.left = self.right
+        else:
+            self.left = self.right
             
-#         if self.right - self.left < self.k:
-#             return False
-#         else:
-#             return True
+        if self.right - self.left < self.k:
+            return False
+        else:
+            return True
         
         
         
