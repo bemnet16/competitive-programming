b7fc50a0fbf2603ed78225f4e6efe19137f1dcef
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-02-18 18:21:19 +0300

    Time: 2763 ms (20.37%), Space: 44 MB (24.26%) - LeetHub

--- 2155-all-divisions-with-the-highest-score-of-a-binary-array/2155-all-divisions-with-the-highest-score-of-a-binary-array.py
+++ 2155-all-divisions-with-the-highest-score-of-a-binary-array/2155-all-divisions-with-the-highest-score-of-a-binary-array.py
@@ -1,62 +1,36 @@
+from collections import defaultdict
+
 class Solution:
     def maxScoreIndices(self, nums: List[int]) -> List[int]:
         
-        n = len(nums)
+        count_1 = 0
         
-        prefix_sum0 = [0]
-        prefix_sum1 = [0]
+        for num in nums:
+            if num == 1:
+                count_1 += 1
         
-        accumulator_0 = 0
-        accumulator_1 = 0
+        scores = defaultdict(list)
+        scores[count_1].append(0)
+        max_score = count_1
+        count_0 = 0
         
-        for i in range(n):
+        for i,num in enumerate(nums):
             
-            if nums[i] == 0:
-                accumulator_0 += 1
-            
-            else:
-                accumulator_1 += 1
-            
-            prefix_sum0.append(accumulator_0)
-            prefix_sum1.append(accumulator_1)
+            count_0 += int(num == 0)
+            count_1 -= int(num == 1)
+            score = count_0 + count_1
+
+            scores[score].append(i + 1)
+            max_score = max(max_score, score)
         
-        res = []
-        for i in range(n + 1):
-            
-            left_0 = 0
-            right_1 = 0
-            
-            if i == 0:
-                right_1 = prefix_sum1[-1]
-            elif i == n:
-                left_0 = prefix_sum0[-1]
-            else:
-                left_0 = prefix_sum0[i]
-                right_1 = prefix_sum1[-1] - prefix_sum1[i]
-                
-            sum_0_1 = left_0 + right_1
-            
-            res.append([i, sum_0_1])
         
-        res.sort(key=lambda x:x[1], reverse=True)
+        return scores[max_score]
+            
+            
+            
+            
         
-        answer = []
-        cur = res[0][1]
         
-        for i in range(len(res)):
-            if res[i][1] == cur:
-                answer.append(res[i][0])
-            else:
-                break
-        
-        return answer
-        
-            
-            
-            
-            
-            
-            
             
             
             
