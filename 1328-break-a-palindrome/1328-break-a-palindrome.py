f1ed67bb54371847a8633786b7ce08fe3219b0aa
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-02-19 15:22:10 +0300

    Time: 31 ms (81.47%), Space: 16.5 MB (76.46%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.py
+++ 1328-break-a-palindrome/1328-break-a-palindrome.py
@@ -1,33 +1,39 @@
 class Solution:
     def breakPalindrome(self, palindrome: str) -> str:
         
-        count_a = 0
-        for p in palindrome:
-            if p == 'a':
-                count_a += 1
-        
+        # there is no way to break a palindrome of single character 
         if len(palindrome) == 1:
             return ""
         
         idx = -1
-        count = 0
-        
+        left_a = 0
         for i,char in enumerate(palindrome):
             
             if char == 'a':
-                count += 1
-                
+                left_a += 1
+             
+            # there is a character other than 'a'
             if ord(char) > 97:
                 idx = i
                 break
+         
         
+        # if all characters are only 'a' idx will be -1
         if idx == -1:
             return palindrome[:(len(palindrome) - 1)] + 'b'
         
-        if (count_a - count) == count and palindrome[idx + 1] == 'a':
+        
+        right_a = 0
+        for i in range(idx,len(palindrome)):
+            if palindrome[i] == 'a':
+                right_a += 1
+        
+        # check if only a singel character is between in all 'a's
+        if left_a == right_a and palindrome[idx + 1] == 'a':
             idx = palindrome.rindex('a')
             return palindrome[:idx] + 'b' + palindrome[(idx + 1):]
         
+        # otherwise return the palindrome string after changing the first non 'a' to 'a'
         return palindrome[:idx] + 'a' + palindrome[(idx + 1):]
             
             
