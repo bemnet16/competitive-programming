bfbcd2a856aace0db4b2069da3cdaa5fbb2b92b9
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-02-19 19:16:10 +0300

    Time: 524 ms (5.01%), Space: 31.6 MB (5.91%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
@@ -1,33 +1,44 @@
 class Solution:
     def sumSubarrayMins(self, arr: List[int]) -> int:
         
-        prefix = {}
-        stk = []
+        n = len(arr)
         
-        for i in range(len(arr)):
-            while stk and arr[stk[-1]] > arr[i]:
-                popped = stk.pop()
-                prefix[popped][1] = i
+        # record the smaller value span of each number /till where is it small value to the right/ 
+        next_span = {}
+        next_stack = []
+        
+        # record the span until which it is smaller to the left
+        previous_span = {}
+        prev_stack = []
+        
+        for i in range(n):
             
-            stk.append(i)
-            prefix[i] = [i, len(arr)]
-        
-        suffix = {}
-        stack = []
-        
-        for i in range(len(arr) - 1, -1, -1):
-            while stack and arr[stack[-1]] >= arr[i]:
-                popped = stack.pop()
-                suffix[popped][1] = i
+            # record the span length of each num until where is it smallest to the right start from itself
+            ### using non-decreasing monotonic stack ###
+            while next_stack and arr[next_stack[-1]] > arr[i]:
+                popped = next_stack.pop()
+                next_span[popped][1] = i
             
-            stack.append(i)
-            suffix[i] = [i, -1]
+            next_stack.append(i)
+            next_span[i] = [i, n]
+            
+            
+            # record the span length of each element till where is it smaller to the left start from its index
+            ### using strictly increasing monotonic stack ###
+            while prev_stack and arr[prev_stack[-1]] >= arr[n - 1 - i]:
+                popped = prev_stack.pop()
+                previous_span[popped][1] = (n - 1 - i)
+            
+            prev_stack.append(n - 1 - i)
+            previous_span[n - 1 - i] = [n - 1 - i, -1]
+        
+        
         
         answer = 0
         for i,v in enumerate(arr):
             
-            left = suffix[i][0] - suffix[i][1]
-            right = prefix[i][1] - prefix[i][0]
+            left = previous_span[i][0] - previous_span[i][1]
+            right = next_span[i][1] - next_span[i][0]
             
             answer += (left * right * v)
         
