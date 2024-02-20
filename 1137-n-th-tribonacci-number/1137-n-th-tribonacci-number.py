68980a7342dc448e297fd686daed1ee3a9241891
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-02-20 10:30:59 +0300

    Time: 45 ms (10.95%), Space: 16.5 MB (65.32%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -0,0 +1,32 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        
+        known_trib = {0:0, 1:1, 2:1}
+        
+        def triBon(n):
+        
+            if n == 0: return 0
+            if n == 1: return 1
+            if n == 2: return 1
+            
+            if (n - 3) not in known_trib:
+                t1 = triBon(n - 3)
+                known_trib[n - 3] = t1
+            else:
+                t1 = known_trib[n - 3]
+            
+            if (n - 2) not in known_trib:
+                t2 = triBon(n - 2)
+                known_trib[n - 2] = t2
+            else:
+                t2 = known_trib[n - 2]
+            
+            if (n - 1) not in known_trib:
+                t3 = triBon(n - 1)
+                known_trib[n - 1] = t3
+            else:
+                t3 = known_trib[n - 1]
+            
+            return t1 + t2 + t3
+        
+        return triBon(n)
