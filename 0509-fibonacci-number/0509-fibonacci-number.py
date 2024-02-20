fd00f59c7b3303106562d6376584198593e4bc76
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-02-20 10:15:32 +0300

    Time: 1204 ms (5.01%), Space: 16.6 MB (50.18%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -3,18 +3,21 @@ class Solution:
         
         d = {0:0, 1:1}
         
-        if n <= 1: return n
+        def f(n):
+            if n <= 1: return n
+
+            if n - 1 not in d:
+                fl = self.fib(n - 1)
+                d[n - 1] = fl
+            else:
+                fl = d[n - 1]
+
+            if n - 2 not in d:
+                fr = self.fib(n - 2)
+                d[n - 2] = fr
+            else:
+                fr = d[n - 2]
+
+            return fl + fr
         
-        if n - 1 not in d:
-            fl = self.fib(n - 1)
-            d[n - 1] = fl
-        else:
-            fl = d[n - 1]
-        
-        if n - 2 not in d:
-            fr = self.fib(n - 2)
-            d[n - 2] = fr
-        else:
-            fr = d[n - 2]
-        
-        return fl + fr
+        return f(n)
