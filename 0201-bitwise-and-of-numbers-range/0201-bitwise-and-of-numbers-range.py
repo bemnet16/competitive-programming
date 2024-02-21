d45286876d9323a7f92d820b89745268cc332a6d
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-02-21 10:59:54 +0300

    Time: 47 ms (80.72%), Space: 16.6 MB (81.69%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.py
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.py
@@ -3,14 +3,30 @@ class Solution:
         
         if left == right: return left
         
-        count = 0
-        while left != right:
+#         count = 0
+#         while left != right:
             
-            left = left >> 1
-            right = right >> 1
-            count += 1
+#             left = left >> 1
+#             right = right >> 1
+#             count += 1
 
-        answer = right * (2 ** count)
-        # answer = right << count
+#         answer = right * (2 ** count)
+#         # answer = right << count
+        
+#         return answer
+
+        
+        left_bits = bin(left)[2:]
+        right_bits = bin(right)[2:]
+        
+        if len(left_bits) != len(right_bits):
+            return 0
+        
+        answer = 0
+        i = 0
+        
+        while left_bits[i] == right_bits[i]:
+            answer += ((2 ** (len(left_bits) - 1 - i)) * (int(left_bits[i])))
+            i += 1
         
         return answer
