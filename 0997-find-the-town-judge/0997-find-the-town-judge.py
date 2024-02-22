56c5aae6a5e010ad9c23824c8d3c01e7de331b65
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-02-22 14:12:50 +0300

    Time: 597 ms (76.99%), Space: 21.5 MB (86.28%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -7,7 +7,11 @@ class Solution:
             return 1
         
         d = defaultdict(int)
+        
+        # store numbers that we haven't seen yet
         s = set()
+        
+        # prefix sum /total/ 
         tot = 0
         
         for a,b in trust:
@@ -15,23 +19,28 @@ class Solution:
                 s.add(a)
                 tot += a
         
-        
+        # expected sum that contains every numbers
         exp = (n * (n + 1)) // 2
         
+        # if all numbers are present there is no judge
         if exp == tot:
             return -1
         
+        # judge will be the missing number 
         judge = exp - tot
         
-        d[judge] = judge
+        
         for a,b in trust:
             if b == judge:
                 d[a] = judge
-            
+                
+        # if judge is a number in 'n' inorder not to affect isJudge 
+        d[judge] = judge
         
-        isJ = all(d[i] == judge for i in range(1, n + 1))
+        # check if there is a least one number that doesn't trust judge 
+        isJudge = all(d[i] == judge for i in range(1, n + 1))
 
-        if d and isJ:
+        if d and isJudge:
             return judge
         
         return -1
