49d03e8d6d1d19593d3e6f589b891420a4b78f19
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-02-26 16:35:16 +0300

    Time: 35 ms (76.23%), Space: 16.6 MB (43.87%) - LeetHub

--- 1849-splitting-a-string-into-descending-consecutive-values/1849-splitting-a-string-into-descending-consecutive-values.py
+++ 1849-splitting-a-string-into-descending-consecutive-values/1849-splitting-a-string-into-descending-consecutive-values.py
@@ -10,15 +10,10 @@ class Solution:
                 if (cur + 1) == pre_val and backtrack(i + 1, cur):
                     return True
             return False
-                
-        
+
         
         for i in range(len(s) - 1):
             cur = int(s[:(i + 1)])
-            
-            if cur == 0:
-                continue
-                
             if backtrack(i + 1, cur):
                 return True
         
