79a916900632960e7ccb5efc2e777f83d1833e75
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-02-26 20:49:55 +0300

    Add solution for Fibonacci Number

---
+++ leetcode/fibonacci-number.py
@@ -0,0 +1,24 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        
+        d = {0:0, 1:1}
+        
+        def f(n):
+            
+            if n <= 1: return n
+            
+            if n - 1 not in d:
+                fl = f(n - 1)
+                d[n - 1] = fl
+            else:
+                fl = d[n - 1]
+
+            if n - 2 not in d:
+                fr = f(n - 2)
+                d[n - 2] = fr
+            else:
+                fr = d[n - 2]
+
+            return fl + fr
+
+        return f(n)
