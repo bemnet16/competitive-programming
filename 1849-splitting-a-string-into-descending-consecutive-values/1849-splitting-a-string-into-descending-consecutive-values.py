54755cd52294528ad59ff68c3e4489fd797c6e57
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-02-26 16:28:18 +0300

    Time: 41 ms (38.73%), Space: 16.5 MB (61.27%) - LeetHub

--- 1849-splitting-a-string-into-descending-consecutive-values/1849-splitting-a-string-into-descending-consecutive-values.py
+++ 1849-splitting-a-string-into-descending-consecutive-values/1849-splitting-a-string-into-descending-consecutive-values.py
@@ -1,13 +1,13 @@
 class Solution:
     def splitString(self, s: str) -> bool:
         
-        def backtrack(idx, val):
+        def backtrack(idx, pre_val):
             if idx == len(s):
                 return True
             
             for i in range(idx, len(s)):
                 cur = int(s[idx:(i + 1)])
-                if (cur + 1) == val and backtrack(i + 1, cur):
+                if (cur + 1) == pre_val and backtrack(i + 1, cur):
                     return True
             return False
                 
@@ -15,6 +15,10 @@ class Solution:
         
         for i in range(len(s) - 1):
             cur = int(s[:(i + 1)])
+            
+            if cur == 0:
+                continue
+                
             if backtrack(i + 1, cur):
                 return True
         
