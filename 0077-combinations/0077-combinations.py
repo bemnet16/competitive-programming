22323292b824cbb91a182435070b4e3face49e4e
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-02-26 11:27:39 +0300

    Time: 700 ms (79.70%), Space: 64.8 MB (74.10%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,44 +1,46 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         
-        nums = [i for i in range(1, (n + 1))]
-        answer = []
-        
-        def backtrack(i, path):
-            
-            if len(path) == k:
-                answer.append(path.copy())
-                return
-            
-            if i >= n:
-                return
-            
-            path.append(nums[i])
-            backtrack(i + 1, path)
-            path.pop()
-            
-            backtrack(i + 1, path)
-        
-        backtrack(0, [])
-        return answer
-        
-        
-        
-        
-        
+#         nums = [i for i in range(1, (n + 1))]
 #         answer = []
         
-#         def backtrack(start, path):
+#         def backtrack(i, path):
             
 #             if len(path) == k:
 #                 answer.append(path.copy())
 #                 return
             
-#             for num in range(start, n + 1):
-#                 path.append(num)
-#                 backtrack(num + 1, path)
-#                 path.pop()
+#             if i >= n:
+#                 return
             
-#         backtrack(1, [])
+#             path.append(nums[i])
+#             backtrack(i + 1, path)
+#             path.pop()
+            
+#             backtrack(i + 1, path)
+        
+#         backtrack(0, [])
 #         return answer
+        
+        
+        
+        
+        
+        answer = []
+        
+        def backtrack(start, path):
+            
+            if len(path) == k:
+                answer.append(path.copy())
+                return
+            
+            for num in range(start, n + 1):
+                path.append(num)
+                backtrack(num + 1, path)
+                path.pop()
+            
+            return
+            
+        backtrack(1, [])
+        return answer
         
