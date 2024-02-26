13fd916c4145ead8d53aef226e0f97579d2b9555
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-02-26 16:47:32 +0300

    Time: 5183 ms (11.22%), Space: 16.5 MB (64.32%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
@@ -2,21 +2,28 @@ class Solution:
     def distributeCookies(self, cookies: List[int], k: int) -> int:
         
         answer = float("inf")
+        mx = float("-inf")
         
         def backtrack(index, distrib):
+            
             nonlocal answer
+            nonlocal mx
             
             if index >= len(cookies):
-                answer = min(answer, max(distrib))
+                answer = min(answer, mx)
                 return
             
-            if max(distrib) >= answer:
+            if mx >= answer:
                 return
             
             for person in range(k):
                 distrib[person] += cookies[index]
+                p_mx = mx
+                mx = max(mx, distrib[person])
                 backtrack(index + 1, distrib)
                 distrib[person] -= cookies[index]
+                mx = p_mx
+                
             
         
         backtrack(0, [0] * k)
