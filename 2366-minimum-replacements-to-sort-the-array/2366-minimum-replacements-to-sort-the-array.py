424ab7ee2a964a1a49effac8b0a3a138c307e252
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-02-27 14:43:54 +0300

    Time: 416 ms (75.32%), Space: 31.2 MB (58.53%) - LeetHub

--- 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.py
+++ 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.py
@@ -2,16 +2,14 @@ class Solution:
     def minimumReplacement(self, nums: List[int]) -> int:
         
         answer = 0
-        current_value = nums[-1]
+        temp = nums[-1]
         
-        for i in range(len(nums) - 2, -1, -1):
-            
-            if nums[i] > current_value:
-                space = ceil(nums[i] / current_value)  # divde optimaly within the space
-                answer += (space - 1)      # perfomed space - 1 operations
-                current_value = nums[i] // space  # the minimum value after the operation
-                
-            else:
-                current_value = nums[i]
+        for i in range(len(nums) - 1, -1, -1):
+            space = ceil(nums[i] / temp)
+            answer += (space - 1)
+            temp = nums[i] // space
         
         return answer
+            
+        
+       
