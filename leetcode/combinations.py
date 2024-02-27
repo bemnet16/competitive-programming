aed131a6e277c3d5e75c047d1896aa6a14591a76
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-02-27 14:33:25 +0300

    Add solution for Combinations

---
+++ leetcode/combinations.py
@@ -0,0 +1,46 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        
+#         nums = [i for i in range(1, (n + 1))]
+#         answer = []
+        
+#         def backtrack(i, path):
+            
+#             if len(path) == k:
+#                 answer.append(path.copy())
+#                 return
+            
+#             if i >= n:
+#                 return
+            
+#             path.append(nums[i])
+#             backtrack(i + 1, path)
+#             path.pop()
+            
+#             backtrack(i + 1, path)
+        
+#         backtrack(0, [])
+#         return answer
+        
+        
+        
+        
+        
+        answer = []
+        
+        def backtrack(start, path):
+            
+            if len(path) == k:
+                answer.append(path.copy())
+                return
+            
+            for num in range(start, n + 1):
+                path.append(num)
+                backtrack(num + 1, path)
+                path.pop()
+            
+            return
+            
+        backtrack(1, [])
+        return answer
+        
