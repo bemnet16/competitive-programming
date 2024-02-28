c448be2cf7a3bfc90a58632b7282517022ce896c
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-02-28 23:11:26 +0300

    Time: 43 ms (70.67%), Space: 17.3 MB (61.23%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+        left = 0
+        right = len(s) - 1
+        
+        while left <= right:
+            
+            if (not (s[left].isalpha() or s[left].isdigit())) or (s[left] == " "):
+                left += 1
+            
+            elif (not (s[right].isalpha() or s[right].isdigit())) or (s[right] == " "):
+                right -= 1
+            
+            elif s[left].lower() != s[right].lower():
+                return False
+            
+            else:
+                left += 1
+                right -= 1
+        
+        return True
+            
+            
+        
