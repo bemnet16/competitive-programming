786743ac4676a6d5e32d22d83a7c5ec46e8963a6
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-02-28 13:52:46 +0300

    Time: 108 ms (47.58%), Space: 17.4 MB (91.93%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,62 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        
+        n = numbers.copy()
+        n.sort()
+        
+        left = 0
+        right = len(n) - 1
+        
+        while left < right:
+            
+            cur_sum = n[left] + n[right]
+            
+            if cur_sum < target:
+                left += 1
+            elif cur_sum > target:
+                right -= 1
+            else:
+                return [(left + 1), (right + 1)]
+        
+        
+#         left = 0
+#         right = 1
+        
+#         while right < len(numbers):
+            
+#             cur_sum = numbers[left] + numbers[right]
+#             if cur_sum == target:
+#                 return [(left + 1), (right + 1)]
+            
+#             elif cur_sum > target:
+#                 left += 1
+#                 right = left + 1
+            
+#             elif right == len(numbers) - 1:
+#                 left += 1
+#                 right = left + 1
+                
+#             else:   
+#                 right += 1
+        
+        
+        
+#         left, right = 0, 1
+        
+#         while right < len(numbers):
+            
+#             s_temp = numbers[left] + numbers[right]
+            
+#             if s_temp < target and right == len(numbers) - 1:
+#                 left += 1
+#                 right = left + 1
+            
+#             elif s_temp < target:
+#                 right += 1
+            
+#             elif s_temp > target:
+#                 left += 1
+            
+#             else:
+#                 return [(left + 1), (right + 1)]
+        
