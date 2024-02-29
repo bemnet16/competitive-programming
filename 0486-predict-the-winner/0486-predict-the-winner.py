a648ba8e0881188a8347957a7cbf92fb24330997
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-02-29 18:03:21 +0300

    Time: 41 ms (61.70%), Space: 17 MB (51.03%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -0,0 +1,29 @@
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        
+        @cache
+        def maxScore(left, right, turn):
+            
+            if left > right:
+                return 0
+            
+            
+            if turn:
+                
+                option_1 = nums[left] + maxScore(left + 1, right, not turn) 
+                option_2 = nums[right] + maxScore(left, right - 1, not turn)
+            
+                return max(option_1, option_2)
+            
+            
+            else:
+                
+                option_1 = -nums[left] + maxScore(left + 1, right, not turn)
+                option_2 = -nums[right] + maxScore(left, right - 1, not turn)
+                
+                return min(option_1, option_2)
+        
+        
+        score = maxScore(0, len(nums) - 1, True)
+        
+        return score >= 0
