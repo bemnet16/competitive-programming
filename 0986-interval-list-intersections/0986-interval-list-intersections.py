644d0c1f59ee3d38685eea89a65edd2174988af4
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-02-29 11:23:18 +0300

    Time: 130 ms (27.45%), Space: 17.8 MB (29.01%) - LeetHub

---
+++ 0986-interval-list-intersections/0986-interval-list-intersections.py
@@ -0,0 +1,42 @@
+from collections import OrderedDict
+
+class Solution:
+    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
+        
+        answer = OrderedDict()
+        
+        f_pointer = 0
+        s_pointer = 0
+        
+        while f_pointer < len(firstList) and s_pointer < len(secondList):
+            
+            f_start, f_end = firstList[f_pointer]
+            s_start, s_end = secondList[s_pointer]
+            
+            if s_start <= f_start and f_end <= s_end:
+                answer[(f_start, f_end)] = (f_start, f_end)
+            
+            elif f_start <= s_start and s_end <= f_end:
+                answer[(s_start, s_end)] = (s_start, s_end)
+            
+            elif s_start <= f_start and f_start <= s_end:
+                answer[(f_start, s_end)] = (f_start, s_end)
+            
+            elif f_start <= s_start and s_start <= f_end:
+                answer[(s_start, f_end)] = (s_start, f_end)
+            
+            
+            if f_end < s_end:
+                f_pointer += 1
+            
+            elif f_end > s_end:
+                s_pointer += 1
+            
+            else:
+                if f_start < s_start:
+                    f_pointer += 1
+                else:
+                    s_pointer += 1
+        
+        return answer.keys()
+        
