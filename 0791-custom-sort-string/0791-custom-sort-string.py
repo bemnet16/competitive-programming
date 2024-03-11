e2fce3c79f041576bcff4647f7347395ea6c627f
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-03-11 22:07:46 +0300

    Time: 17 ms (35.99%), Space: 11.6 MB (70.92%) - LeetHub

---
+++ 0791-custom-sort-string/0791-custom-sort-string.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def customSortString(self, order, s):
+        
+        answer = []
+        s_set = defaultdict(int)
+        
+        for char in s:
+            s_set[char] += 1
+        
+        
+        for char in order:
+            
+            if s_set[char] > 0:
+                answer.append(char * s_set[char])
+                del s_set[char]
+        
+        
+        for k in s_set:
+            answer.append(k * s_set[k])
+        
+        
+        
+        return "".join(answer)
