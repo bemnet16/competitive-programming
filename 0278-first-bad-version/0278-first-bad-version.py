5f9ecd34af11bb08d414249b4e907ded85634be8
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-03-13 19:01:06 +0300

    Time: 30 ms (83.95%), Space: 16.4 MB (97.15%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.py
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -4,24 +4,21 @@
 class Solution:
     def firstBadVersion(self, n: int) -> int:
         
-        first_bad = float("inf")
-        
-        left = 0
+        left = 1
         right = n
         
-        while left <= right:
+        while left < right:
             
-            mid = (left + right) // 2
+            mid = left + (right - left) // 2
             
             if isBadVersion(mid):
-                first_bad = min(first_bad, mid)
-                right = mid - 1
+                right = mid
             else:
                 left = mid + 1
         
         
         
-        return first_bad
+        return right
                 
             
         
