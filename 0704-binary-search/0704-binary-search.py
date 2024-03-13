8779f508f787c9a0d8e50db97394255f098487a7
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-03-13 18:25:13 +0300

    Time: 207 ms (5.88%), Space: 25.4 MB (6.93%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -2,10 +2,10 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         
         
-        left = 0
-        right = len(nums) - 1
-        
-        while left <= right:
+        def binarySearch(left, right):
+            
+            if left > right:
+                return -1
             
             mid = (left + right) // 2
             
@@ -13,10 +13,11 @@ class Solution:
                 return mid
             
             if nums[mid] < target:
-                left = mid + 1
+                return binarySearch(mid + 1, right)
             
-            else:
-                right = mid - 1
+            return binarySearch(left, mid - 1)
         
         
-        return -1
+        
+        
+        return binarySearch(0, len(nums) - 1)
