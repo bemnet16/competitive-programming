534769d92d8fb4f3d6f0d55632f8c3ec650d744e
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-03-15 23:45:40 +0300

    Add solution for Replace the Substring for Balanced String

---
+++ leetcode/replace-the-substring-for-balanced-string.py
@@ -0,0 +1,39 @@
+class Solution:
+    def balancedString(self, s: str) -> int:
+        
+        char_occurence = {"Q":0, "W": 0, "E": 0, "R":0}
+        balanced_num = len(s) // 4
+        
+        for char in s:
+            char_occurence[char] += 1
+        
+        
+        if all(char_occurence[char] == balanced_num for char in char_occurence):
+            return 0
+        
+        
+        def checkBalance(temp_occurence):
+
+            return all(char_occurence[char] - temp_occurence[char] <= balanced_num for char in char_occurence)
+        
+        
+        
+        min_length = len(s)
+        left = 0
+        temp_occurence = defaultdict(int)
+        
+        
+        for right, char in enumerate(s):
+            
+            temp_occurence[char] += 1
+            
+            while checkBalance(temp_occurence):
+                
+                min_length = min(min_length, (right - left + 1))
+                temp_occurence[s[left]] -= 1
+                left += 1
+        
+        
+        return min_length
+            
+            
