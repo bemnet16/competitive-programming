58d4fd40785344501576f4f5ed3d95176133793e
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-03-16 15:36:15 +0300

    Time: 621 ms (53.08%), Space: 22.3 MB (33.26%) - LeetHub

---
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        
+        
+        count = 0
+        maxlen = 0
+        track = {0: -1}
+        
+        for i in range(len(nums)):
+            
+            count += -1 if nums[i] == 0 else 1
+            
+            if count in track:
+                maxlen = max(maxlen, (i - track[count]))
+        
+            if count not in track:
+                track[count] = i
+                
+                
+        return maxlen
+            
+            
