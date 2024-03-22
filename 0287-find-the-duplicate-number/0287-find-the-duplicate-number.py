6cd6f972c5868486a48464bb36a65f7a2aab250d
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-03-22 19:52:41 +0300

    Time: 504 ms (20.73%), Space: 30.5 MB (58.06%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,6 +1,20 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         
+        
+        for i in range(len(nums)):
+            
+            while i != (nums[i] - 1):
+                
+                temp = nums[i] - 1
+                if nums[i] == nums[temp]:
+                    return nums[i]
+                
+                else:
+                    nums[i], nums[temp] = nums[temp], nums[i]
+        
+        
+        
         # Length of nums is (n + 1)
 #         n = len(nums) - 1
         
@@ -11,9 +25,9 @@ class Solution:
 #         return actual_sum - expected_sum
         
     
-        num_set = set()
-        for num in nums:
-            if num in num_set:
-                return num
-            num_set.add(num)
+#         num_set = set()
+#         for num in nums:
+#             if num in num_set:
+#                 return num
+#             num_set.add(num)
         
