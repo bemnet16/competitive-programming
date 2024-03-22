24e4bac2c149c638d1c5f96a5d3fa1cbd5a48604
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-03-22 18:55:41 +0300

    Time: 107 ms (78.18%), Space: 17.7 MB (76.91%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,30 +1,34 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         
-        nums.append(len(nums))
+        n = len(nums)
+        expected_sum = (n * (n + 1)) // 2
+        actual_sum = sum(nums)
+
+        # The differce is the missing number
+        return expected_sum - actual_sum
+        
+        
+        #### Cyclic sort approach ####
+#         nums.append(len(nums))
     
-        for i in range(len(nums)):
+#         for i in range(len(nums)):
 
-            while i != nums[i] and nums[i] != nums[nums[i]]:
+#             while i != nums[i] and nums[i] != nums[nums[i]]:
 
-                temp = nums[i]
-                nums[i], nums[temp] = nums[temp], nums[i]
+#                 temp = nums[i]
+#                 nums[i], nums[temp] = nums[temp], nums[i]
 
-        for i, num in enumerate(nums):
+#         for i, num in enumerate(nums):
 
-            if i != nums[i]:
-                return i
+#             if i != nums[i]:
+#                 return i
 
-        return len(nums) - 1
+#         return len(nums) - 1
 
     
     
-#         n = len(nums)
-#         expected_sum = (n * (n + 1)) // 2
-#         actual_sum = sum(nums)
 
-#         # The differce is the missing number
-#         return expected_sum - actual_sum
 
 
         
