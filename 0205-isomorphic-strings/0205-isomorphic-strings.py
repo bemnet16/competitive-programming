b341cb6a414c71461b754cab4c0ca299933bfbf5
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-03-28 01:41:56 +0300

    Time: 45 ms (43.20%), Space: 16.8 MB (64.73%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,19 +1,34 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         
-        charmap = defaultdict(str)
-        reserved = set()
+        map_st, map_ts = defaultdict(str), defaultdict(str)
         
-        for i in range(len(s)):
+        for char_s, char_t in zip(s, t):
             
-            if s[i] not in charmap and t[i] not in reserved:
-                charmap[s[i]] = t[i]
-                reserved.add(t[i])
+            if (char_s in map_st and map_st[char_s] != char_t) or (char_t in map_ts and map_ts[char_t] != char_s):
+                return False
+            
+            map_st[char_s] = char_t
+            map_ts[char_t] = char_s
+        
+        
+        return True
+            
+            
+        
+#         charmap = defaultdict(str)
+#         reserved = set()
+        
+#         for i in range(len(s)):
+            
+#             if s[i] not in charmap and t[i] not in reserved:
+#                 charmap[s[i]] = t[i]
+#                 reserved.add(t[i])
           
         
-        chars = []
+#         chars = []
         
-        for c in s:
-            chars.append(charmap[c])
+#         for c in s:
+#             chars.append(charmap[c])
         
-        return "".join(chars) == t
+#         return "".join(chars) == t
