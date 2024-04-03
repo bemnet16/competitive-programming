ae9b80b71236ad5b28449835e2f18edd6a8c1667
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-04-03 23:25:20 +0300

    Time: 53 ms (6.36%), Space: 17 MB (55.27%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -8,56 +8,25 @@ class Solution:
             
             mid = low + (high - low) // 2
             
-            if nums[low] > nums[high]:
-                if nums[mid] > nums[high]:
+            if nums[mid] == target:
+                return mid
+            
+            elif nums[low] <= nums[mid]:
+                
+                if target < nums[low] or target > nums[mid]:
                     low = mid + 1
                 
                 else:
-                    high = mid
+                    high = mid - 1
             
-            elif nums[low] < nums[high]:
-                if nums[mid] < nums[high]:
+            
+            else:
+                
+                if target < nums[mid] or target > nums[high]:
                     high = mid - 1
                 
                 else:
-                    low = mid
-            
-            else:
-                break
-                
-                
-        
-        if nums[low] == target:
-            return low
-        
-        if len(nums) == 1:
-            return -1
+                    low = mid + 1
         
         
-        
-        if nums[0] <= target and low > 0 and target <= nums[low - 1]:
-            high = low - 1
-            low = 0
-            
-        else:
-            low = low + 1
-            high = len(nums) - 1
-            
-            
-            
-        
-        while low <= high:
-            
-            mid = low + (high - low) // 2
-            
-            if target < nums[mid]:
-                high = mid - 1
-            elif nums[mid] < target:
-                low = mid + 1
-            else:
-                return mid
-                    
-        
-        return high if nums[high] == target else -1
-                
-        
+        return -1
