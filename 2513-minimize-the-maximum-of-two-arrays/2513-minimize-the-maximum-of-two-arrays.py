7e3f548e82f753341d202553bd43e65512416f39
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-04-08 23:03:32 +0300

    Time: 31 ms (84.62%), Space: 16.4 MB (94.18%) - LeetHub

---
+++ 2513-minimize-the-maximum-of-two-arrays/2513-minimize-the-maximum-of-two-arrays.py
@@ -0,0 +1,28 @@
+import math
+
+class Solution:
+    def minimizeSet(self, divisor1: int, divisor2: int, uniqueCnt1: int, uniqueCnt2: int) -> int:
+        
+        def canMake(num):
+            que1 = num - (num // divisor1)
+            que2 = num - (num // divisor2)
+            que_12 = num - (num // math.lcm(divisor1, divisor2))
+            
+            return que1 >= uniqueCnt1 and que2 >= uniqueCnt2 and que_12 >= (uniqueCnt1 + uniqueCnt2)
+        
+        
+        low = 0
+        high = 10 ** 10
+        
+        while low < high:
+            
+            mid = low + (high - low) // 2
+            
+            if canMake(mid):
+                high = mid
+            
+            else:
+                low = mid + 1
+        
+        
+        return low
