922f43df23caab4802f0e2781144e2f7dba9af14
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-04-08 23:58:14 +0300

    Time: 24 ms (98.54%), Space: 16.5 MB (77.45%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.py
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.py
@@ -3,41 +3,19 @@ import math
 class Solution:
     def kthFactor(self, n: int, k: int) -> int:
         
-        for i in range(1, int(math.sqrt(n)) + 1):
-            
-            if not (n % i):
+        for i in range(1, math.ceil(math.sqrt(n))):
+            if  n % i == 0:
                 k -= 1
             
-            if not k:
+            if k == 0:
                 return i
         
         for i in range(int(math.sqrt(n)), 0, -1):
-            
-            if not (n % (n // i)):
+            if (n % (n // i)) == 0:
                 k -= 1
             
-            if not k:
+            if k == 0:
                 return n // i
         
         return -1
     
-    import math 
-class Solution: 
-    def kthFactor(self, n: int, k: int) -> int:
-        root = math.sqrt(n)
-        i = 1
-        while i < root:
-            if n % i == 0: 
-                k -= 1
-            if k == 0: 
-                return i
-            i += 1
-        for j in range(int(root), 0, -1):
-            if n % (n//j) == 0: 
-                k -= 1
-            if k == 0: 
-                return n//j
-        return -1   
-                
-            
-        
