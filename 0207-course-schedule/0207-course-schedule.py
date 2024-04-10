bc68d6d8b8df4a7382ce4ef62aa21f0ed907c8a5
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-04-10 21:56:15 +0300

    Time: 88 ms (68.26%), Space: 18.6 MB (11.96%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,33 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
+        GRAPH = defaultdict(set)
+        
+        for course, preReq in prerequisites:
+            GRAPH[course].add(preReq)
+            
+        colors = [0 for _ in range(numCourses)]
+        
+        def dfs(node):
+            
+            colors[node] = 1
+            
+            if node in GRAPH:
+                for neighbour in GRAPH[node]:
+                    if colors[neighbour] == 1:
+                        return False
+                    if colors[neighbour] == 0:
+                        if not dfs(neighbour):
+                            return False
+            
+            colors[node] = 2
+            return True
+            
+        
+        
+        
+        for course in GRAPH:
+            if colors[course] == 0:
+                if not dfs(course):
+                    return False
+        return True
