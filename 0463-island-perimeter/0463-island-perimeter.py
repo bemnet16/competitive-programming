f3fa2042953a9845f8495f4363e3a919428ce8e2
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-04-10 10:37:30 +0300

    Time: 483 ms (20.69%), Space: 19 MB (24.82%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -2,28 +2,56 @@ class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         
         DIRECTIONS = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+        VISITED = [[False for _ in range(len(grid[0]))] for _ in range(len(grid))]
+        
         
         def inbound(row, col):
             
             return (0 <= row < len(grid)) and (0 <= col < len(grid[0]))
         
         
-        perimeter = 0
+        answer = 0
+        def dfs(row, col):
+            
+            nonlocal answer
+            
+            if not grid[row][col]:
+                answer += 1
+                return
+            
+            
+            VISITED[row][col] = True
+            
+            for row_change, col_change in DIRECTIONS:
+                
+                new_row = row + row_change
+                new_col = col + col_change
+                
+                bounded = inbound(new_row, new_col)
+                
+                if bounded and not VISITED[new_row][new_col]:
+                    dfs(new_row, new_col)
+                
+                elif not bounded:
+                    answer += 1
+        
+        index = []
         for row in range(len(grid)):
             for col in range(len(grid[0])):
-                
-                if not grid[row][col]:
-                    continue
-                
-                for row_chg, col_chg in DIRECTIONS:
-                    new_row = row + row_chg
-                    new_col = col + col_chg
-                    
-                    if inbound(new_row, new_col) and not grid[new_row][new_col]:
-                        perimeter += 1
-                    
-                    elif not inbound(new_row, new_col):
-                        perimeter += 1
+                if grid[row][col]:
+                    index = [row, col]
         
-        
-        return perimeter
+
+        if index: dfs(index[0], index[1])
+        return answer
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
