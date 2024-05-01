eacce8f550a82a0cf99f161c29feee45e8edd079
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-05-01 04:24:10 -0400

    Time: 40 ms (38.02%), Space: 16.7 MB (35.04%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -4,3 +4,16 @@ class Solution:
         return bin(int(a, 2) + int(b, 2))[2:]
         
         
+        # if len(a) < len(b):
+        #     a="0"*(len(b)-len(a)) + a
+        # elif len(a) > len(b):
+        #     b="0"*(len(a)-len(b)) + b
+        # r=0
+        # s=""
+        # for i in range(len(a)-1,-1,-1):
+        #     s+=str((int(a[i])+int(b[i])+r)%2)
+        #     r=(int(a[i])+int(b[i])+r)//2
+        # if r!=0: s+=str(r)
+        # s=s[::-1]
+        # return s
+        
