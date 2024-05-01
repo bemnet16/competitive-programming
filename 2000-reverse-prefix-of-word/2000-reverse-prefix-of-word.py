77f8016842b8719d76a46652dcb346ba147f4e7b
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-05-01 06:30:16 -0400

    Time: 40 ms (18.93%), Space: 16.5 MB (46.70%) - LeetHub

---
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reversePrefix(self, word: str, ch: str) -> str:
+        
+        if ch not in word:
+            return word
+        
+        index = word.index(ch)
+        
+        return word[:index + 1][::-1] + word[index + 1:]
+        
