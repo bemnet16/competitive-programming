11cffa7549620cbc7f9480ea388c69ba4d093e53
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-05-08 12:23:13 -0400

    Time: 38 ms (61.29%), Space: 16.5 MB (77.42%) - LeetHub

--- 1147-longest-chunked-palindrome-decomposition/1147-longest-chunked-palindrome-decomposition.py
+++ 1147-longest-chunked-palindrome-decomposition/1147-longest-chunked-palindrome-decomposition.py
@@ -3,27 +3,34 @@ class Solution:
         
         left = 0
         right = len(text) - 1
-        ans = 0
+        k = 0
         
         while left <= right:
             
             cur = text[right]
-            f = True
+            equal_subtext = False  ## check if the subtext is identifcal from both side(left & right)
+            length = 0  ## length of the subtext
             
-            count = 0
-            
-            while left < len(text) and (f):
-                count += 1
+            while left < len(text) and not equal_subtext:
+                
+                length += 1
+                
                 if text[left] == cur:
-                    if text[left - count + 1: left + 1] == text[right - count + 1: right + 1]:
-                        f = False
-                        if left <= right - count:
-                            ans += 2
+                    
+                    if text[left - length + 1: left + 1] == text[right - length + 1: right + 1]:
+                        
+                        if left <= right - length:
+                            k += 2
+
                         else:
-                            ans += 1
-                        right -= count
+                            k += 1
+                        
+                        equal_subtext = True
+                        right -= length
                 
                 left += 1
         
-        return ans
+        
+        
+        return k
         
