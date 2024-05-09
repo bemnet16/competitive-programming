ab9ef636d50df826ed973a3c10b1332da98269e0
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-05-09 12:26:52 -0400

    Time: 36 ms (54.44%), Space: 16.6 MB (12.20%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,33 +1,28 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
-        money = [None] * len(nums)
+        if len(nums) <= 1:
+            return nums[0]
+        
+        money = [-1] * len(nums)
+        
         
         def dp(i):
         
-            if i >= len(nums):
-                return 0
-            
             max_money = 0
             
-            for j in range(i + 2, len(nums)):
+            for house in range(i + 2, len(nums)):
                 
-                if  money[j]:
-                    max_money = max(max_money, money[j])
+                if  money[house] != -1:
+                    max_money = max(max_money, money[house])
                     
                 else:
-                    dp_j = dp(j)
-                    money[j] = dp_j
-                    max_money = max(max_money, dp_j)
+                    dp_house = dp(house)
+                    money[house] = dp_house
+                    max_money = max(max_money, dp_house)
             
             return nums[i] + max_money
         
         
         
-        ans = 0
-        for i in range(len(nums)):
-            if nums[i] != 0:
-                ans = max(ans, dp(i))
-        
-        
-        return ans
+        return max(dp(0), dp(1))
