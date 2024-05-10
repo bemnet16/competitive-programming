0c6454a5a12a005eef8d73de7726dba8cba32e62
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-05-10 11:28:25 -0400

    Time: 795 ms (16.55%), Space: 27.6 MB (11.22%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,53 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        ans = 0
+        stk = []
+        
+        i = 0
+        while i < len(prices):
+            
+            while stk and prices[i] < stk[-1]:
+                stk.pop()
+            
+            while i < len(prices) and stk and stk[-1] <= prices[i]:
+                ans = max(ans, (prices[i] - stk[-1]))
+                i += 1
+            
+            if i < len(prices):
+                stk.append(prices[i])
+        
+        return ans
+        
+        
+        
+#         cache = [None] * len(prices)
+#         cache[-1] = 0
+        
+#         def dp(i):
+            
+#             nonlocal ans
+            
+#             if i >= len(prices):
+#                 return 0
+            
+#             mx = 0
+#             for j in range(i + 1, len(prices)):
+#                 if cache[j] != None:
+#                     dp_j = cache[j]
+#                 else:
+#                     dp_j = dp(j)
+#                     cache[j] = dp_j
+                    
+#                 mx = max(mx, prices[j] - prices[i]) 
+            
+#             ans = max(ans, mx)
+#             return prices[i]
+        
+#         ans = 0
+#         # for stock in range(len(prices)):
+#         #     if prices[stock] != 0 and prices[stock] > ans:
+#         dp(0)
+            
+        
+#         return ans
