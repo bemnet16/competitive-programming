74d226099412c017c3b09229e6fa37a910c860f0
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-05-15 12:11:52 -0400

    Time: 818 ms (7.70%), Space: 26 MB (99.58%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -2,93 +2,11 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
         ans = 0
-        heap = [prices[-1]]
         
         for i in range(len(prices) - 2, -1, -1):
             
-            ans = max(ans, (heap[0] - prices[i]))
-            heappushpop(heap, prices[i])
-            
-            
+            ans = max(ans, prices[i + 1] - prices[i])
+            prices[i] = max(prices[i], prices[i + 1])
+        
+        
         return ans
-    
-    
-        
-#         ans = 0
-#         stk = []
-        
-#         i = 0
-#         while i < len(prices):
-            
-#             while stk and prices[i] < stk[-1]:
-#                 stk.pop()
-            
-#             while i < len(prices) and stk and stk[-1] <= prices[i]:
-#                 ans = max(ans, (prices[i] - stk[-1]))
-#                 i += 1
-            
-#             if i < len(prices):
-#                 stk.append(prices[i])
-        
-#         return ans
-        
-        
-        
-#         def dp(i, profit, state):
-            
-#             nonlocal ans
-            
-#             if i >= len(prices):
-#                 return
-            
-            
-#             if state == "N":
-#                 dp(i + 1, profit - prices[i], "B")
-#                 dp(i + 1, profit, "N")
-            
-            
-#             elif state == "B":
-#                 ans = max(ans, (profit + prices[i]))
-#                 dp(i + 1, profit, "B")
-            
-            
-            
-            
-        
-        
-#         ans = 0
-#         dp(0, 0, "N")
-#         return ans
-            
-
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
