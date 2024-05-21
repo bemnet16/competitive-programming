1b5f2eb6f4edb77e7ff4be5f5f5f1b0d4e783166
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-05-21 09:17:43 -0400

    Time: 373 ms (23.90%), Space: 17.6 MB (93.70%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -1,48 +1,106 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
         
-        cache = {}
+        m, n = len(matrix), len(matrix[0])
+        
+        DIRECTIONS = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+        indegree = [[0 for _ in range(n)] for _ in range(m)]
         
         def inbound(row, col):
-            return (0 <= row < len(matrix)) and (0 <= col < len(matrix[0]))
+            return (0 <= row < m) and (0 <= col < n)
         
-        def dp(row, col):
-            
-            if not inbound(row, col):
-                return 0
-            
-            if (row, col) in cache:
-                return cache[(row, col)]
-            
-            
-            left = 1
-            if inbound(row, col + 1) and matrix[row][col] < matrix[row][col + 1]:
-                left = 1 + dp(row, col + 1)
+        for row in range(m):
+            for col in range(n):
                 
-            right = 1
-            if inbound(row, col - 1) and matrix[row][col] < matrix[row][col - 1]:
-                right = 1 + dp(row, col - 1)
-                
-            up = 1
-            if inbound(row - 1, col) and matrix[row][col] < matrix[row - 1][col]:
-                up = 1 + dp(row - 1, col)
-                
-            down = 1
-            if inbound(row + 1, col) and matrix[row][col] < matrix[row + 1][col]:
-                down = 1 + dp(row + 1, col)
-            
-            
-            cache[(row, col)] = max(left, right, up, down)
-            
-            return cache[(row, col)]
+                for row_chg, col_chg in DIRECTIONS:
+                    new_row = row + row_chg
+                    new_col = col + col_chg
+                    
+                    if inbound(new_row, new_col) and matrix[row][col] > matrix[new_row][new_col]:
+                        indegree[row][col] += 1
+                        
         
         
+        queue = deque()
+        for row in range(m):
+            for col in range(n):
+                if indegree[row][col] == 0:
+                    queue.append((row, col))
         
-        long_path = 1
-        for row in range(len(matrix)):
-            for col in range(len(matrix[0])):
-                long_path = max(long_path, dp(row, col))
+        
+        print(indegree, queue)
+        long_path = 0
+        while queue:
+            
+            for i in range(len(queue)):
                 
+                row, col = queue.popleft()
+                for row_chg, col_chg in DIRECTIONS:
+                    new_row = row + row_chg
+                    new_col = col + col_chg
+                    
+                    if inbound(new_row, new_col) and matrix[row][col] < matrix[new_row][new_col]:
+                        indegree[new_row][new_col] -= 1
+                        if indegree[new_row][new_col] == 0:
+                            queue.append((new_row, new_col))
+            
+            long_path += 1
+        
+        
         return long_path
+                    
+
+
+
+
+
+
+
+
+
+
+#         cache = {}
+        
+#         def inbound(row, col):
+#             return (0 <= row < len(matrix)) and (0 <= col < len(matrix[0]))
+        
+#         def dp(row, col):
+            
+#             if not inbound(row, col):
+#                 return 0
+            
+#             if (row, col) in cache:
+#                 return cache[(row, col)]
+            
+            
+#             left = 1
+#             if inbound(row, col + 1) and matrix[row][col] < matrix[row][col + 1]:
+#                 left = 1 + dp(row, col + 1)
+                
+#             right = 1
+#             if inbound(row, col - 1) and matrix[row][col] < matrix[row][col - 1]:
+#                 right = 1 + dp(row, col - 1)
+                
+#             up = 1
+#             if inbound(row - 1, col) and matrix[row][col] < matrix[row - 1][col]:
+#                 up = 1 + dp(row - 1, col)
+                
+#             down = 1
+#             if inbound(row + 1, col) and matrix[row][col] < matrix[row + 1][col]:
+#                 down = 1 + dp(row + 1, col)
+            
+            
+#             cache[(row, col)] = max(left, right, up, down)
+            
+#             return cache[(row, col)]
+        
+        
+        
+#         long_path = 1
+#         for row in range(len(matrix)):
+#             for col in range(len(matrix[0])):
+#                 long_path = max(long_path, dp(row, col))
+                
+#         return long_path
             
         
