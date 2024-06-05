fd4d06af4f2f0d89fdb66c5021cd8938514ec824
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-06-05 11:15:34 -0900

    Time: 38 ms (49.70%), Space: 16.4 MB (95.70%) - LeetHub

---
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.py
@@ -0,0 +1,18 @@
+class Codec:
+    def __init__(self):
+        self.encodeMap = {}
+        self.decodeMap = {}
+
+    def encode(self, longUrl: str) -> str:
+        """Encodes a URL to a shortened URL.
+        """
+        if longUrl not in self.encodeMap: 
+            shortUrl = 'http://' + str(len(self.encodeMap) + 1)
+            self.encodeMap[longUrl] = shortUrl
+            self.decodeMap[shortUrl] = longUrl
+        return self.encodeMap[longUrl]
+
+    def decode(self, shortUrl: str) -> str:
+        """Decodes a shortened URL to its original URL.
+        """
+        return self.decodeMap[shortUrl]
