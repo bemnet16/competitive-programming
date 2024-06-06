33b4a96aad3c9c6cfe86c18c4209971427252aed
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-06-06 03:45:33 -0900

    Time: 4937 ms (27.04%), Space: 16.2 MB (99.79%) - LeetHub

---
+++ 0079-word-search/0079-word-search.py
@@ -0,0 +1,38 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        
+        DIRECTIONS = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+        
+        def inbound(row, col):
+            return (0 <= row < len(board)) and (0 <= col < len(board[0]))
+        
+        def dfs(vis, row, col, i):
+            
+            if i >= len(word):
+                return True
+            
+            for row_chg, col_chg in DIRECTIONS:
+                new_row = row + row_chg
+                new_col = col + col_chg
+                
+                if (new_row, new_col) not in vis and inbound(new_row, new_col) and board[new_row][new_col] == word[i]:
+                    vis.add((new_row, new_col))
+                    if dfs(vis, new_row, new_col, i + 1):
+                        return True
+                    vis.remove((new_row, new_col))
+            return False
+            
+        
+        
+        
+        
+        for row in range(len(board)):
+            for col in range(len(board[0])):
+                if board[row][col] == word[0]:
+                    if dfs({(row, col)}, row, col, 1):
+                        return True
+        
+        return False
+                
+        
+        
