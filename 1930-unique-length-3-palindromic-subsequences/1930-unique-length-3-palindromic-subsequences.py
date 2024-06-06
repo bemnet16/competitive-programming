a08630cbc4cea93b6b5bcb9957ad7e992c1b0e54
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-06-06 05:28:48 -0900

    Time: 531 ms (46.34%), Space: 17.4 MB (55.22%) - LeetHub

---
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countPalindromicSubsequence(self, s: str) -> int:
+        
+        indices = defaultdict(list)
+        for i, char in enumerate(s):
+            if char not in indices:
+                indices[char] = [i, i]
+            indices[char][1] = i
+        
+        
+        ans = 0
+        for start, end in indices.values():
+            
+            count = set()
+            for i in range(start + 1, end):
+                count.add(s[i])
+            
+            ans += len(count)
+        
+        
+        return ans
