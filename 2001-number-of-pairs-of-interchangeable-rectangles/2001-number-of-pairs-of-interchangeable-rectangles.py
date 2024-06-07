4c41626ac2baefd062e641b7678dc96648b94d8e
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-06-07 22:25:45 +0300

    Time: 1332 ms (30.95%), Space: 72.7 MB (80.80%) - LeetHub

--- 2001-number-of-pairs-of-interchangeable-rectangles/2001-number-of-pairs-of-interchangeable-rectangles.py
+++ 2001-number-of-pairs-of-interchangeable-rectangles/2001-number-of-pairs-of-interchangeable-rectangles.py
@@ -1,23 +1,14 @@
 class Solution:
     def interchangeableRectangles(self, rectangles: List[List[int]]) -> int:
         
-        def get_gcf(a, b):
-            if b == 0:
-                return a
-            return get_gcf(b, (a % b))
-        
-        
-        
         counts = defaultdict(int)
         ans = 0
         
         for i in range(len(rectangles) - 1, -1, -1):
             
             width, height = rectangles[i]
-            gcf = get_gcf(width, height)
-            
-            ans += counts[(width // gcf, height // gcf)]
-            counts[(width // gcf, height // gcf)] += 1
+            ans += counts[(width / height)]
+            counts[(width /height)] += 1
         
         
         return ans
