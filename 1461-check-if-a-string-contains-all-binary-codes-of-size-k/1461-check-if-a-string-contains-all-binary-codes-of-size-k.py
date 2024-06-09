fad026f03e14265e7f6c9a20d15ad8f48edf0e3a
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-06-09 13:05:37 +0300

    Time: 261 ms (73.58%), Space: 53.7 MB (48.34%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
@@ -5,14 +5,19 @@ class Solution:
         
         for i in range(len(s) - k + 1):
             hashMap.add(s[i:i + k])
-        
-        
-        for i in range(1 << k):
             
-            bins = bin(i)[2:].zfill(k)
-            if bins not in hashMap:
-                return False
+        
+        return len(hashMap) == (1 << k)
         
         
-        return True
+#         for i in range(1 << k):
+            
+#             bins = bin(i)[2:].zfill(k)
+#             if bins not in hashMap:
+#                 return False
+        
+        
+#         return True
+
+
         
