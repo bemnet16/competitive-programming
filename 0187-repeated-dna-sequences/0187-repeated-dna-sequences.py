c4aa929b379c806c10ebfd59f1497692e60a58e4
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-06-09 10:24:53 +0300

    Time: 88 ms (8.19%), Space: 28.7 MB (21.09%) - LeetHub

---
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+
+        answer = set()
+        occurence = set()
+        occurence.add(s[:10])
+        
+        
+        for i in range(1, len(s) - 10 + 1):
+            
+            subString = s[i: i + 10]
+            
+            if subString in occurence:
+                answer.add(subString)
+                
+            else:
+                occurence.add(subString)
+        
+        
+        return answer
+        
+        
+        
