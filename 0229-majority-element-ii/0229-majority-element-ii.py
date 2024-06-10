6574c3858d9b6167cf33baf6e46f2cb02f5acad3
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-06-10 16:08:15 +0300

    Time: 96 ms (71.28%), Space: 17.9 MB (20.20%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -3,13 +3,25 @@ from collections import Counter
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
         
-        counter = Counter(nums)
+        counter = defaultdict(int)
+        
+        for num in nums:
+            
+            counter[num] += 1
+            if len(counter) <= 2:
+                continue
+            
+            new_counter = defaultdict(int)
+            for n in counter:
+                if counter[n] > 1:
+                    new_counter[n] = counter[n] - 1
+                    
+            counter = new_counter
+            
         
         answer = []
-        q = len(nums)/3
-        
-        for num, count in counter.items():
-            if count > q:
+        for num in counter:
+            if nums.count(num) > len(nums) / 3:
                 answer.append(num)
         
         return answer
