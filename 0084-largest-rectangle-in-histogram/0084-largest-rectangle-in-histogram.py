170b99ef333c8d8d02808eb66784a68311042da0
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-06-25 18:47:08 +0300

    Time: 677 ms (41.75%), Space: 33.6 MB (42.96%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -8,15 +8,13 @@ class Solution:
         
         for i, height in enumerate(heights):
             
-            top = None
+            start = i
             while stack and stack[-1][1] > height:
                 top = stack.pop()
+                start = top[0]
                 ans = max(ans, top[1] * (i - top[0]))
             
-            if top:
-                stack.append((top[0], height))
-            else:
-                stack.append((i, height))
+            stack.append((start, height))
         
         return ans
         
