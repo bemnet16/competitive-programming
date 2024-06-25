50ce939223b74a8711420f316744b9314070ac96
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-06-25 15:51:13 +0300

    Time: 60 ms (51.86%), Space: 18.6 MB (29.86%) - LeetHub

---
+++ 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def convertBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        
+        def dfs(root):
+            
+            nonlocal cur
+            
+            if root:
+                dfs(root.right)
+                root.val += cur
+                cur = root.val
+                dfs(root.left)
+            
+            
+        cur = 0
+        dfs(root)
+        return root
