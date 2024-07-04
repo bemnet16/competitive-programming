7c28cd0560e69c87c3b53af78cec020fbcf7fce0
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-07-04 11:31:43 +0300

    Time: 682 ms (97.08%), Space: 53.3 MB (92.20%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.py
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.py
@@ -6,21 +6,21 @@
 class Solution:
     def mergeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
-        merged = ListNode()
-        merged_head = merged
         cur_sum = 0
+        merge_point = head
         cur = head.next
         
         while cur:
             cur_sum += cur.val
             
             if cur.val == 0:
-                merged_head.next = ListNode(cur_sum)
-                merged_head = merged_head.next
+                merge_point = merge_point.next
+                merge_point.val = cur_sum
                 cur_sum = 0
             
             cur = cur.next
         
         
-        return merged.next
+        merge_point.next = None
+        return head.next
         
