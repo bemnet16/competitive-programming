ff4035aeea7e076f9762c75a08cccb49f3d12ad6
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-08-03 08:14:22 -0900

    Time: 70 ms (81.01%), Space: 16.8 MB (59.96%) - LeetHub

---
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.py
@@ -0,0 +1,7 @@
+class Solution:
+    def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
+        
+        if sorted(target) == sorted(arr):
+            return True
+        return False
+        
