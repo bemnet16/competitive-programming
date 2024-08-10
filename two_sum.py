6a7f4953078b015a4481ac6a3cb9b600c9a934fe
Bemnet Adugnaw <bemnet.adugnaw@aait.edu.et>
2024-08-10 13:41:16 +0300

    Create two_sum.py

---
+++ two_sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        track = {}
+        
+        for i, num in enumerate(nums):
+            
+            if (target - num) in track:
+                return [track[target - num], i]
+            
+            track[num] = i
