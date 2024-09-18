329380bc6e503b61e508f217fa4258c05c3453f0
Bemnet Adugnaw <bemnetadugnaw@gmail.com>
2024-09-18 22:29:03 +0300

    Time: 44 ms (36.28%), Space: 16.5 MB (32.99%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.py
@@ -0,0 +1,18 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+
+        for i,num in enumerate(nums):
+            nums[i]=str(num)
+
+        def check(n1,n2):
+            if n1+n2 > n2+n1:
+                return -1
+            else:
+                return 1
+
+        nums= sorted(nums,key=cmp_to_key(check))
+
+        return str(int(''.join(nums)))
+        
+
+        
